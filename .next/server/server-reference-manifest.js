9d65a86373cf34520715bfd900332d8d6c91970c
Felipe Agustín Alejandro Reyes Jaramillo <felipe.reys2003@gmail.com>
2024-06-03 02:26:46 -0400

    cambie casi todo xd (todo esta comentado)

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"hX+FpgtzLxg+SFlU1VwAZXMbaeLVruvLMBAzhxIfgBg=\"\n}"
--- app/page.js
+++ app/page.js
@@ -1,106 +1,48 @@
 "use client"
 import { useState } from "react";
 import './globals.css';
+import useImageUploader from "./useImageUploader";
 
-//Se definen las varibles con el useState, para actualizar constantemente el estado de cada variable
 export default function Home() {
-  const [file, setFile] = useState(null);
-  const [ancho, setWidth] = useState('');
-  const [alto, setHeight] = useState('');
-  const [previewUrl, setPreview] = useState('');
+  const { imageData, handleImageUpload } = useImageUploader();
+  const [error, setError] = useState(null);
+  const [imageDimensions, setImageDimensions] = useState({ ancho: 0, alto: 0 });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!file) {
-      alert('Por favor seleccione un archivo para subir');
-      return;
+  //esta funcion se ejecuta cuando el usuario elige una imagen en el input
+  const onFileChange = async (event) => {
+    //obtiene el archivo seleccionado
+    const file = event.target.files[0];
+    try {
+      //await handleImageUpload(file) llama a handleImageUpload y espera a que la promesa se cumpla
+      const dataUrl = await handleImageUpload(file);
+      //creamos un objeto Image para poder obtener sus dimensiones
+      const img =new Image();
+      img.onload = () => {
+        setImageDimensions({ancho: img.width, alto: img.height})
+      };
+      img.src=dataUrl;
+    //si hay un error durante la subida, se actualiza el estado a error. 
+    } catch (err) {
+      setError(err);
     }
   };
-  
-  //Funciones para prevenir errores y caídas
-const handleFileSubmit = (e) => {
-    e.preventDefault();
-  };
-
-const handleSizeSubmit = (e) => {
-    e.preventDefault();
-    console.log("Ancho:", ancho, "Alto:", alto);
-};
-
-//SUBIR EL ARCHIVO
-  const handleImage = (e) => {
-    const file = e.target.files[0];
-    setFile(file);
-    if (file) {
-      setPreview(URL.createObjectURL(file));
-    }
-  };
-
-  //DESCARGAR LA IMAGEN
-  const handleDownload = () => {
-    if (previewUrl) {
-      const link = document.createElement('a');
-      link.href = previewUrl;
-      link.setAttribute('download', 'imagen');
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    }
-  };
-
+//me dio paja colocar lo del css (no tenia idea como)
   return (
-
     <div className="container">
       <h1>Conversor de imágenes bacano :D </h1>
-    {/* Caja para seleccionar archivo */}
-      <form onSubmit={handleFileSubmit}>
-        <div className="form-section">
-          <label className="custom-file-upload">
-            <input id="file-upload" type="file" onChange={handleImage} />
-            Seleccione un archivo
-          </label>
+      <input type="file" accept="image/*" onChange={onFileChange} />
+      {/*si ocurre un error en la subida del archivo, esta linea muestra un mensaje de error en rojo*/}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {/*si imageData tiene un valor ocurre lo siguiente */}
+      {imageData && (
+        <div>
+          <h3>Preview:</h3>
+          {/*Para mostrarlo en sus valores originales tendriamos que quitar "style={{ maxWidth: '256px'}}" */}
+          <img src={imageData} alt="Uploaded" style={{ maxWidth: '256px' }} />
+          <p>Ancho: {imageDimensions.ancho}px</p>
+          <p>Alto: {imageDimensions.alto}px</p>
         </div>
-        {/* <button type="submit" className="submit-button">Enviar Archivo</button> */}
-      </form>
-
-      <form onSubmit={handleSizeSubmit}> {/*onSubmit se usa para especificar una función que se ejecutará cuando se envíe un formulario*//*handleSizeSubmit es una avriable en la usaremos para manejar eventos del formulario  */}
-        <div className="form-section">
-          <div className="input-container">
-            <label htmlFor="ancho">Anchura:</label>
-            <input
-              id="ancho"
-              type="number"
-              value={ancho}
-              onChange={(e) => {
-                setWidth(Number(e.target.value));
-              }}
-            />
-          </div>
-          <div className="input-container">
-            <label htmlFor="alto">Altura:</label>
-            <input
-              id="alto"
-              type="number"
-              value={alto}
-              onChange={(e) => {
-                setHeight(Number(e.target.value));
-              }}
-            />
-          </div>
-        </div>
-        <button type="submit" className="submit-button">Enviar Dimensiones</button>
-      </form>
-
-      <div className="preview-section">
-        <p>Anchura: {ancho} px</p>
-        <p>Altura: {alto} px</p>
-        {previewUrl && (
-          <div>
-            <img src={previewUrl} alt="Preview" style={{ width: ancho, height: alto }} />
-            <button onClick={handleDownload}>Descargar imagen</button>
-          </div>
-        )}
-      </div>
+      )}
     </div>
   );
 }
